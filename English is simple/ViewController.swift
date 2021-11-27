e07436db7b3995c47f3a593352dcf2e5a0468ca7
Ruslan <ruslangazizov36@gmail.com>
2021-11-27 14:40:01 +0300

    Initial Commit

---
+++ English is simple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  English is simple
+//
+//  Created by Руслан on 27.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
