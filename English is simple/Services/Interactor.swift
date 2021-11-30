eb02a275aaa1d3291747a2a04ae168ede2505cf9
Ruslan <ruslangazizov36@gmail.com>
2021-11-30 22:58:59 +0300

    Add Interactor for services

---
+++ English is simple/Services/Interactor.swift
@@ -0,0 +1,54 @@
+//
+//  Interactor.swift
+//  English is simple
+//
+//  Created by Руслан on 27.11.2021.
+//
+
+import Foundation
+
+final class Interactor {
+
+    // MARK: - Properties
+    private let networkService: NetworkServiceProtocol = NetworkService.shared
+    private let persistableSerice: PersistableServiceProtocol = PersistableService.shared
+    
+    var onboardingShouldBeShown: Bool {
+        return persistableSerice.isFirstOpeningOfTheApplication()
+    }
+    
+    // MARK: - Public methods
+    func getNewWordInfo(named wordName: String, completion: @escaping ([Word]?) -> Void) {
+        let operationQueue = OperationQueue()
+        operationQueue.maxConcurrentOperationCount = 10
+        operationQueue.qualityOfService = .userInitiated
+        operationQueue.addOperation { [weak self] in
+            guard let self = self else { return }
+            self.networkService.getWordInfo(word: wordName) { result in
+                switch result {
+                case .success(let words):
+                    DispatchQueue.main.async {
+                        completion(words)
+                    }
+                case .failure(_):
+                    print("No definitions found for word \(wordName)")
+                }
+            }
+        }
+    }
+    
+    func saveNewWord(word: Word) {
+        persistableSerice.saveWord(word)
+    }
+    
+    func getSavedWords(startWith wordPart: String?, completion: @escaping ([Word]) -> Void) {
+        let words = persistableSerice.fetchWords(beginWith: wordPart)
+        completion(words)
+    }
+    
+    func deleteSavedWord(named wordName: String, completion: @escaping (Bool) -> Void) {
+        let successfullyDeleted = persistableSerice.deleteWord(named: wordName)
+        completion(successfullyDeleted)
+    }
+
+}
